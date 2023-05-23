cd0caa3991791baae08d30ae3cf994e3af5c2f99
Frico Simon <74108779+FricoSimon@users.noreply.github.com>
2023-05-23 17:38:23 +0700

    add time.js

---
+++ time.js
@@ -0,0 +1,18 @@
+console.log("Cara membuat teh manis");
+console.log("Ikuti langkah - langkah berikut:");
+console.log("Siapkan satu cangkir, satu teh celup, dan gula secukupnya");
+console.log("Pertama, masukkan satu buah teh celup ke dalam cangkir");
+console.log("Kedua, tuangkan air panas secukupnya");
+
+const ketiga = () => {
+  console.log(`Tiga, mulai celup dan tarik teh dari dalam cangkir beberapa kali sampai warna air berubah kecoklatan`);
+}
+const keempat = () => {
+  console.log("Lalu, masukkan gula secukupnya");
+}
+const kelima = () => {
+  console.log("Aduk merata dan teh siap dihidangkan!");
+}
+setTimeout(ketiga, 2000);
+setTimeout(keempat, 4000);
+setTimeout(kelima, 6000);
